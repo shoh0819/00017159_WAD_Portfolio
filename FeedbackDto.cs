589aa1f8a203fcc15409b977fa1f4950b47bef17
shoh0819 <sadullayevshohjahon990@gmail.com>
2024-12-02 15:16:30 -0500

    Mapping profile and DTos

---
+++ FeedbackDto.cs
@@ -0,0 +1,13 @@
+﻿namespace _00017159_WAD_Portfolio.DTOs
+{
+    public class FeedbackDto
+    {
+        public int FeedbackId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        
+    }
+}
---
+++ MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using _00017159_WAD_Portfolio.DTOs;
+using _00017159_WAD_Portfolio.Models;
+using AutoMapper;
+
+namespace _00017159_WAD_Portfolio.MappingProfiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Feedback, FeedbackDto>();
+            CreateMap<User, UserDto>();
+        }
+    }
+}
